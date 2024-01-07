18ea59bc7d18a180a7e387129b5fa431e06d36d9
Carlos Eduardo <cpwaldow@gmail.com>
2024-01-07 19:53:11 -0300

    criando o componente Sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import './App.css';
+import About from './components/About';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <main className='container'>
+        <About />
+      </main>
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import './About.css';
+
+const About = () => {
+  return (
+    <section>
+      <h2 className='about--title'>Sobre</h2>
+      <p className='about--description'>
+        Analista de Desenvolvimento de Sistemas na Trybe, com 2 anos de
+        experiência, focado em automação e manutenção de projetos. Especializado
+        em HTML, CSS, JavaScript, React, Typescript e ferramentas como GitHub
+        Actions, ESLint e Cypress. Desenvolvedor Front-end na Reclame Aqui,
+        destacando-se em React, Vue e Angular. Experiência em SEO, Google
+        TagManager e forte habilidade em testes (funcionais, unidade,
+        integração, usabilidade, caixa preta, regressão). Contribuí como Quality
+        Assurance no ReclameAQUI, liderando testes e automação.
+      </p>
+    </section>
+  );
+};
+
+export default About;
